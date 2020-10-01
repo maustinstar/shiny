d844ae3e0abaaa9d9c0188a69b9b587c1ca110e2
ryanslikesocool <ryanjboyer@gmail.com>
2020-10-01 12:14:41 -0600

    Add macOS support

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Shiny",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v14),
+        .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
--- Sources/Shiny/MotionManager.swift
+++ Sources/Shiny/MotionManager.swift
@@ -7,17 +7,28 @@
 
 import SwiftUI
 import CoreMotion
+#if os(macOS)
+import AppKit
+#endif
 
 internal class MotionManager: ObservableObject {
+    #if os(iOS)
     @Published var yaw: CGFloat = 0
     @Published var pitch: CGFloat = 0
     @Published var roll: CGFloat = 0
     
     var motionInput = CMMotionManager()
+    #elseif os(macOS)
+    @Published var locationX: CGFloat = 0
+    @Published var locationY: CGFloat = 0
     
+    var motionInput = NSEvent()
+    #endif
+        
     static var main = MotionManager()
     
     init() {
+        #if os(iOS)
         motionInput.deviceMotionUpdateInterval = 0.2;
         motionInput.startDeviceMotionUpdates()
         motionInput.startDeviceMotionUpdates(to: OperationQueue.current!) { _,_  in
@@ -31,11 +42,20 @@ internal class MotionManager: ObservableObject {
                 self.roll = CGFloat(roll)
             }
         }
+        #elseif os(macOS)
+        NSEvent.addLocalMonitorForEvents(matching: [.mouseMoved]) {
+            let screenWidth = NSScreen.main?.frame.width ?? 1920
+            let screenHeight = NSScreen.main?.frame.height ?? 1080
+            self.locationX = -self.motionInput.locationX.remap(origFrom: 0, origTo: screenWidth, targetFrom: -.pi, targetTo: .pi) / 4
+            self.locationY = self.motionInput.locationY.remap(origFrom: 0, origTo: screenHeight, targetFrom: -.pi, targetTo: .pi) / 4
+            return $0
+        }
+        #endif
     }
 }
 
+#if os(iOS)
 internal extension CMMotionManager {
-    
     var yaw: Double? {
         get {
             return deviceMotion?.attitude.yaw
@@ -54,3 +74,31 @@ internal extension CMMotionManager {
         }
     }
 }
+
+#elseif os(macOS)
+
+internal extension NSEvent {
+    var locationX: CGFloat {
+        return NSEvent.mouseLocation.x
+    }
+    
+    var locationY: CGFloat {
+        return NSEvent.mouseLocation.y
+    }
+}
+
+internal extension CGFloat {
+    func lerp(from: CGFloat, to: CGFloat, rel: CGFloat) -> CGFloat {
+      return ((1 - rel) * from) + (rel * to)
+    }
+    
+    func invLerp(from: CGFloat, to: CGFloat, value: CGFloat) -> CGFloat {
+      return (value - from) / (to - from)
+    }
+    
+    func remap(origFrom: CGFloat, origTo: CGFloat, targetFrom: CGFloat, targetTo: CGFloat) -> CGFloat {
+        let rel = invLerp(from: origFrom, to: origTo, value: self)
+        return lerp(from: targetFrom, to: targetTo, rel: rel)
+    }
+}
+#endif
